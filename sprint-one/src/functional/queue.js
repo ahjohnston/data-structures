29f7aedc1df7d6a98212654a30c17e039802a369
Annie Johnston <annie89@gmail.com>
2021-09-23 19:57:03 -0400

    passed tests for functional: queue & stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,4 +1,4 @@
-var Queue = function() {
+var Queue = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -6,13 +6,45 @@ var Queue = function() {
 
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function (value) {
+    //add the new value at key 0; bump down all other key/values
+    var newStorage = {};
+    newStorage[0] = value;
+    console.log('newS ', newStorage);
+    for (var i = 0; i < Object.keys(storage).length; i++) {
+      newStorage[i + 1] = storage[i];
+      console.log('newStorage i + 1 ', newStorage[i + 1]);
+      console.log('storage[i] ', storage[i]);
+    }
+    storage = newStorage;
+    console.log('enqueue result ', storage);
+    console.log('newStorage ', newStorage);
+    return storage;
   };
 
-  someInstance.dequeue = function() {
+  //problem: whenever enqueue() is invoked, the enqueued value populates as .dequeue
+  //problem! whenever enqueue() is invoked, the previously dequeued item is forgotten and is now undefined. Why?
+  someInstance.dequeue = function () {
+    //remove the last key/value
+    var dequeued = Object.values(storage)[Object.keys(storage).length - 1];
+    delete storage[Object.keys(storage).length - 1];
+    return dequeued;
+    // var newStorage = {};
+
+    // for (var key in storage) {
+    //   if (Object.keys(newStorage).length === Object.keys(storage).length - 1) {
+    //     var dequeued = storage[key];
+    //     storage = newStorage;
+    //     // console.log('fdq', dequeued);
+    //     return dequeued;
+    //   }
+    //   newStorage.key = storage.key;
+    // }
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    //return the length of the Object.keys array
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,31 @@
-var Stack = function() {
+var Stack = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function (value) {
+    storage[Object.keys(storage).length] = value;
+    return storage;
+
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function () {
+    var newStorage = {};
+    for (var i = 0; i < Object.keys(storage).length; i++) {
+      if (i === Object.keys(storage).length - 1) {
+        var popped = storage[i];
+        storage = newStorage;
+        return popped;
+      }
+      newStorage[i] = storage[i];
+    }
+
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
